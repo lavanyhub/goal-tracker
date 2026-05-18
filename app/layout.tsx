56999f57866d4887aaca581f3a53b62f8fe5708b
lavanyhub <lavanysharma2023@gmail.com>
2026-05-19 02:31:54 +0530

    remove custom cursor

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
 import Navigation from './navigation'
-import CustomCursor from './components/CustomCursor'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -19,7 +18,6 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <CustomCursor />
         <Navigation />
         {children}
       </body>
